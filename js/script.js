e6badc0255539e2d835512e98ae7e46328d22c5e
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2023-06-11 14:50:32 +0300

    new branch

--- js/script.js
+++ js/script.js
@@ -1,59 +1,59 @@
-const firstName = prompt('Напишите ваше имя?');
-console.log(firstName);
+// const firstName = prompt('Напишите ваше имя?');
+// console.log(firstName);
 
-function user() {
-  let age = prompt('Возраст?', 20);
+// function user() {
+//   let age = prompt('Возраст?', 20);
   
-  if (age < 20) {
-    alert(`Привет, ${firstName}!`);
-  } else if (age >= 20 && age < 45) {
-    alert(`Здравствуй, ${firstName}!`);
-  } else  if (age >= 45){
-    alert(`Здравствуйте, ${firstName}!`);
-  }
+//   if (age < 20) {
+//     alert(`Привет, ${firstName}!`);
+//   } else if (age >= 20 && age < 45) {
+//     alert(`Здравствуй, ${firstName}!`);
+//   } else  if (age >= 45){
+//     alert(`Здравствуйте, ${firstName}!`);
+//   }
     
-}
-user();
+// }
+// user();
 
-const students = ['Aнтон', 'Иван', 'Борис', 'Влад', 'Григорий'];
-for (let i = 0; i < students.length; i++) {
- let x = prompt("Имя студента");
- if (x) {
-  alert(`${x}`);
+// const students = ['Aнтон', 'Иван', 'Борис', 'Влад', 'Григорий'];
+// for (let i = 0; i < students.length; i++) {
+//  let x = prompt("Имя студента");
+//  if (x) {
+//   alert(`${x}`);
 
- } else {
-  alert(`${students}.`);
- }
+//  } else {
+//   alert(`${students}.`);
+//  }
   
-}
-function checkCart (goodsQuantity, totalCost, promoCode) {
-  let finalCost;
-  let discount_1;
-  let discount_2;
-  let discount_3;
+// }
+// function checkCart (goodsQuantity, totalCost, promoCode) {
+//   let finalCost;
+//   let discount_1;
+//   let discount_2;
+//   let discount_3;
   
 
-  if ( goodsQuantity > 10) {
-    discount_1 = (totalCost / 100) * 5;
-  } else if (goodsQuantity > 20) {
-    discount_1 = (totalCost / 100) * 10;
-  }
-  if (totalCost > 10000) {
-    discount_2 = 1000;
-  }
-  if (promoCode) {
-    discount_3 = (totalCost / 100) * 15;
-  }
-  discount = discount_1 + discount_2 + discount_3;
-  finalCost = totalCost - discount;
+//   if ( goodsQuantity > 10) {
+//     discount_1 = (totalCost / 100) * 5;
+//   } else if (goodsQuantity > 20) {
+//     discount_1 = (totalCost / 100) * 10;
+//   }
+//   if (totalCost > 10000) {
+//     discount_2 = 1000;
+//   }
+//   if (promoCode) {
+//     discount_3 = (totalCost / 100) * 15;
+//   }
+//   discount = discount_1 + discount_2 + discount_3;
+//   finalCost = totalCost - discount;
 
 
-  return finalCost
-}
-checkCart();
+//   return finalCost
+// }
+// checkCart();
 
 
-console.log(checkCart(12, 11000, true));
+// console.log(checkCart(12, 11000, true));
 //finalCost === 7800
 
 
